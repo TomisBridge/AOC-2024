ac99a630227c6ed668d2f261e4fa29849db78d85
TomisBridge <116310872+TomisBridge@users.noreply.github.com>
2024-12-19 20:39:01 +0000

    filtering data for problem half complete

--- day3/day3.py
+++ day3/day3.py
@@ -1,3 +1,5 @@
+import re
+
 def main():
     
     data = format_AOC_day3(input("input name: "))
@@ -12,15 +14,23 @@ def main():
 
     print(allow_data)
 
-    allow_char_list = allow_data.split(")"e)
+    allow_char_list = allow_data.replace("m", " m").split(" ")
 
     print(allow_char_list)
+
+    filtered = []
+    for item in allow_char_list:
+        if re.match(item, r"mul(2,4)"):
+            filtered.append(item)
+
+    print(filtered)
 def format_AOC_day3(target_file):
 
     #import data and store it in data as a str
     my_file = open(target_file, "r")
     data = []
     data = my_file.read()
+
 
     # close file
     my_file.close()
