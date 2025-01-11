1efd06c0b2cb5588026277c105b826dfa2e04c05
TomisBridge <116310872+TomisBridge@users.noreply.github.com>
2025-01-11 23:33:36 +0000

    day 5 part 2 complete

--- day5/day5_part2.py
+++ day5/day5_part2.py
@@ -14,20 +14,51 @@ def main():
     if rules == "q":
         sys.exit("\n ---------- Program ended ---------- \n")
 
-       
+    # itterate all rules through all list. if update modified append to end of list and replace with []
+
+    #set variable
+    fail_list = updates
+    list_len = len(updates)
+    counter = 0
+    change = 0
+
+    # itterate through updates
+    for update in fail_list:
+        counter += 1
+        change = 0
+        
+        #itterate through rules
+        for rule in rules:
+
+            # if the update failes the rule swap the two items and append to end of list
+            if if_used(rule, update):
+                fail_list[fail_list.index(update)] = [0]
+                fail_list.append(swap(rule, update)) 
+                change += 1
+
+        # if there are been not modification done to the update then replace it with []
+        if counter <= list_len and change == 0:
+            fail_list[fail_list.index(update)] = [0]
+
+    # create a list of the middle number of all list items
     pass_list = []
-    for update in updates:
-        if if_used(rules, update):
-            pass_list.append(update[int(len(update)/2)])
+    for update in fail_list:
+        pass_list.append(update[int(len(update)/2)])
+    
+    # print the sum of the pass_list items
     print(sum(pass_list))
 
-def if_used(rules, update):
+# check if the update fails the rule
+def if_used(rule, update):
     update_str = " ".join(map(str, update))
-    for rule in rules:
-        pattern = f".*{rule[1]}.*{rule[0]}.*"
-        if re.match(pattern, update_str):
-            return False
-    return True
+    pattern = f".*{rule[1]}.*{rule[0]}.*"
+    if re.match(pattern, update_str):
+        return True
+    return False
 
+# swap the two impropperly ordered update entries
+def swap(rule, update):
+    update[update.index(rule[0])], update[update.index(rule[1])] = update[update.index(rule[1])], update[update.index(rule[0])]
+    return update
 
 main()
